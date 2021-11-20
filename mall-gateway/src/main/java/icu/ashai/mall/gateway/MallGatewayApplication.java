1770a8adbb5d3be49692900b8cccb5d11843df94
Ashai <ashai.cn@gmail.com>
2021-11-20 23:13:15 +0800

    feat： 添加网关服务

---
+++ mall-gateway/src/main/java/icu/ashai/mall/gateway/MallGatewayApplication.java
@@ -0,0 +1,19 @@
+package icu.ashai.mall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 网关服务
+ * @author Ashai
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallGatewayApplication.class, args);
+    }
+
+}
