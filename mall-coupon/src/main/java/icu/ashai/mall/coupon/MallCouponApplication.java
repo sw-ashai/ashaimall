5af7d7461f05c291b060b827c8ab03a64a194cbb
Ashai <ashai.cn@gmail.com>
2021-11-20 15:49:23 +0800

    revert: 还原主启动类

---
+++ mall-coupon/src/main/java/icu/ashai/mall/coupon/MallCouponApplication.java
@@ -0,0 +1,18 @@
+package icu.ashai.mall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ashai
+ * @email ashai.cn@gmail.com
+ * @date 2021-11-20 15:48:10
+ */
+@SpringBootApplication
+public class MallCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallCouponApplication.class, args);
+    }
+
+}
