e837b924a67d3f9899382a506709420a0b91be62
Ashai <ashai.cn@gmail.com>
2022-02-23 16:07:05 +0800

    feat: 添加PubSub前端插件

--- renren-fast-vue/src/main.js
+++ renren-fast-vue/src/main.js
@@ -3,6 +3,7 @@ import App from '@/App'
 import router from '@/router'                 // api: https://github.com/vuejs/vue-router
 import store from '@/store'                   // api: https://github.com/vuejs/vuex
 import VueCookie from 'vue-cookie'            // api: https://github.com/alfhen/vue-cookie
+import PubSub from 'pubsub-js'
 import '@/element-ui'                         // api: https://github.com/ElemeFE/element
 import '@/icons'                              // api: http://www.iconfont.cn/
 import '@/element-ui-theme'
@@ -22,6 +23,7 @@ if (process.env.NODE_ENV !== 'production') {
 // 挂载全局
 Vue.prototype.$http = httpRequest // ajax请求方法
 Vue.prototype.isAuth = isAuth     // 权限方法
+Vue.prototype.PubSub = PubSub    //组件发布订阅消息
 
 // 保存整站vuex本地储存初始状态
 window.SITE_CONFIG['storeState'] = cloneDeep(store.state)
