7e97647eff57c40c2344ce366004ab92d22a8999
ashai <ashai.cn@gmail.com>
2022-07-18 22:02:30 +0800

    feat: 添加登录页面和注册页面，功能未完成

---
+++ mall-auth-server/src/main/java/icu/ashai/mall/auth/config/AshaiMallWebConfig.java
@@ -0,0 +1,20 @@
+package icu.ashai.mall.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author ashai
+ * @date 2022/7/18 21:56
+ * @email ashai@gmail.com
+ * @description
+ */
+@Configuration
+public class AshaiMallWebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/reg.html").setViewName("reg");
+    }
+}
